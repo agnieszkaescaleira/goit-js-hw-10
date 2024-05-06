01b8a8cab211898d93aff19a6ecd64de0ab29376
agnieszkaescaleira <agnieszka.escaleira@gmail.com>
2024-05-06 21:02:51 +0200

    zadanie10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,7 @@ export const fetchCountries = name => {
     const properties = 'fields=name,capital,population,flags,languages';
 
     return fetch(`${BASE_URL}${name}?${properties}`).then(response => {
-        console.log(!response.ok);
+        console.log(!response.ok);s
         if (!response.ok) {
             throw new Error(response.status);
         }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
-import { fetchCountries } from './fetchCountries'; 
+import { fetchCountries } from './fetchCountries.js'; 
 
 const DEBOUNCE_DELAY = 300; 
 
